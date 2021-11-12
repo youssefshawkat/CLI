9c0de3b47883d419aaa61b3aedbec9e6ace12045
youssefshawkat <68171635+youssefshawkat@users.noreply.github.com>
2021-11-12 23:21:42 +0200

    2nd Phase

--- Parser.java
+++ Parser.java
@@ -3,7 +3,7 @@ public class Parser {
     String commandName;
     String[] args;
     String[] str;
-    String[] commands = {"echo"};
+    String[] commands = {"echo","mkdir","cd","pwd","rmdir","ls","ls -r"};
 
     //This method will divide the input into commandName and args
 //where "input" is the string command entered by the user
@@ -28,6 +28,58 @@ public class Parser {
             }
 
         }
+
+
+        if(str[0].equals("mkdir") && args.length ==0){
+
+
+            System.out.println("mkdir: missing operand");
+            return false;
+        }
+        if(str[0].equals("cd") && args.length > 1){
+
+
+            System.out.println("cd: too many arguments");
+
+            return false;
+        }
+
+        if(str[0].equals("rmdir") && args.length > 1){
+
+
+            System.out.println("rmdir: too many arguments");
+
+            return false;
+        }
+
+        if(str[0].equals("ls") && str.length > 1){
+
+
+            str[0] = "ls -r";
+            commandName =str[0];
+
+        }
+
+        if(str[0].equals("ls") && args.length > 0){
+
+
+            System.out.println("ls: too many arguments");
+
+            return false;
+        }
+
+
+        if(commandName.equals("ls -r") && args.length > 1){
+
+
+            System.out.println("ls -r: too many arguments");
+
+            return false;
+        }
+
+
+
+
 
         return true;
 
